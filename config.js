a5922720f6293f555c224eafa919449fed813051
NathanGeraldino <127454900+NathanGeraldino@users.noreply.github.com>
2026-06-12 10:24:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // 2. Cole aqui a URL e a anon key do projeto
 // 3. Rode o SQL do arquivo supabase.sql no SQL Editor do Supabase
 
-const SUPABASE_URL = "COLE_AQUI_A_URL_DO_SUPABASE";
-const SUPABASE_ANON_KEY = "COLE_AQUI_A_ANON_KEY_DO_SUPABASE";
+const SUPABASE_URL = "https://xylfgkixtxigswlhtqnd.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "sb_publishable_oN6iZQJktcSNlfcTBU1y3g_Yu-ihAGe";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
